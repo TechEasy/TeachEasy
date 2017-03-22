979f28850cff02e6d738435fd3ee5eaab25b2eae
Manuel <manuel.coronil1@gmail.com>
2017-03-22 17:04:08 +0100

    Listado de cursos 40%

---
+++ TeachEasy1.0/src/main/java/controllers/Academy/AcademyCourseController.java
@@ -0,0 +1,136 @@
+
+package controllers.Academy;
+
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.AcademyService;
+import services.CourseService;
+import controllers.AbstractController;
+import domain.Academy;
+import domain.Course;
+
+@Controller
+@RequestMapping("/academy/course")
+public class AcademyCourseController extends AbstractController {
+
+	//Services-------------------------
+
+	@Autowired
+	private AcademyService	academyService;
+
+	@Autowired
+	private CourseService	courseService;
+
+
+	//Constructor----------------------
+
+	public AcademyCourseController() {
+		super();
+	}
+
+	// Listing ----------------------------------------------------------------		
+	@RequestMapping(value = "/listCourse", method = RequestMethod.GET)
+	public ModelAndView listCourse() {
+		ModelAndView result;
+		Collection<Course> courses;
+		Academy academy;
+		academy = academyService.findByPrincipal();
+
+		courses = academy.getCourses();
+
+		result = new ModelAndView("course/listCourse");
+		result.addObject("requestURI", "course/academy/listCourse.do");
+		result.addObject("courses", courses);
+
+		return result;
+	}
+
+	// Creation ----------------------------------------------------------------
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView result;
+		Course course;
+
+		course = courseService.create();
+		result = createEditModelAndView(course);
+
+		return result;
+	}
+
+	// Edition ----------------------------------------------------------------		
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam int courseId) {
+		ModelAndView result;
+		Course course;
+
+		course = courseService.findOne(courseId);
+
+		Assert.notNull(course);
+		result = createEditModelAndView(course);
+
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid @ModelAttribute Course course, BindingResult bindingResult) {
+		ModelAndView result;
+
+		if (bindingResult.hasErrors()) {
+			result = createEditModelAndView(course);
+
+		} else {
+
+			try {
+				Course c;
+				c = courseService.save(course);
+				result = new ModelAndView("redirect:edit.do?courseId=" + c.getId());
+
+			} catch (Throwable oops) {
+				result = createEditModelAndView(course, "course.commit.error");
+			}
+		}
+
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
+	public ModelAndView delete(@ModelAttribute Course course, BindingResult bindingResult) {
+		ModelAndView result;
+
+		try {
+			courseService.delete(course);
+			result = new ModelAndView("redirect:listCourse.do");
+		} catch (Throwable oops) {
+			result = createEditModelAndView(course, "course.commit.error");
+		}
+
+		return result;
+	}
+
+	public ModelAndView createEditModelAndView(Course course) {
+		ModelAndView result;
+		result = createEditModelAndView(course, null);
+
+		return result;
+	}
+
+	public ModelAndView createEditModelAndView(Course course, String message) {
+		ModelAndView result;
+		result = new ModelAndView("course/edit");
+		result.addObject("course", course);
+		result.addObject("message", message);
+		return result;
+	}
+}
---
+++ TeachEasy1.0/src/main/webapp/views/course/listCourse.jsp
@@ -0,0 +1,46 @@
+<%--
+ * list.jsp
+ *
+ * Copyright (C) 2017 Universidad de Sevilla
+ * 
+ * The use of this project is hereby constrained to the conditions of the 
+ * TDG Licence, a copy of which you may download from 
+ * http://www.tdg-seville.info/License.html
+ --%>
+
+<%@page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+
+<%@taglib prefix="jstl"	uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@taglib prefix="security" uri="http://www.springframework.org/security/tags"%>
+<%@taglib prefix="display" uri="http://displaytag.sf.net"%>
+
+<display:table  name="courses" id="courseList" pagesize="5" requestURI="course/academy/listCourse.do" class="displaytag">
+	
+	<spring:message code="course.title" var="titleHeader"/>
+	<display:column property="title" title="${titleHeader}"/>
+	
+	<spring:message code="course.createMoment" var="createMomentHeader"/>
+	<display:column property="createMoment" title="${createMomentHeader}" sortable="true"/>
+	
+	<spring:message code="course.updateMoment" var="updateMomentHeader"/>
+	<display:column property="updateMoment" title="${updateMomentHeader}" sortable="true"/>
+	
+	<spring:message code="course.rate" var="rateHeader"/>
+	<display:column property="rate" title="${rateHeader}" sortable="true"/>
+	
+	<spring:message code="course.duration" var="durationHeader"/>
+	<display:column property="duration" title="${durationHeader}" sortable="true"/>
+	
+	<spring:message code="course.level" var="levelHeader"/>
+	<display:column property="level" title="${levelHeader}" sortable="true"/>
+	
+	<spring:message code="course.matter" var="matterHeader"/>
+	<display:column property="subjectMatter.name" title="${matterHeader}" sortable="true"/>
+	
+	<display:column>
+		<a href="course/academy/edit.do?courseId=${courseList.id}"><spring:message	code="course.edit" /></a>
+	</display:column>
+	
+</display:table>
